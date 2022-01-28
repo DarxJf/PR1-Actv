fcee27dbfe22a0b9043e58e44e849285a6b3cb76
DarxJf <jose.mgsanchez0305@gmail.com>
2022-01-27 21:00:39 -0400

    Hello world with ncurses.h, please compile the program with cc and not gcc

--- Hola-mundo.c
+++ Hola-mundo.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <ncurses.h>
+#include <unistd.h>
 
-int main(){
-
-  printf("Hello world\n");
-
+int main(void){
+  initscr();
+  for(int i=2;i>=0;i--){
+  printw("Hello world\n");
+  refresh();
+  sleep(2);
+  clear();
+  printw("Hola mundoo");
+  refresh();
+  sleep(2);
+  clear();
+  }
+  endwin();
   return 0;
 }
